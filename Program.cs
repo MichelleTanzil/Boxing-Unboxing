3ceb6a9bade67a70e53d8dab48d5a8cc7f004560
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-23 15:06:53 -0800

    added comments for readability

--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@ namespace BoxingAndUnboxing
     {
         static void Main(string[] args)
         {
+            // Create an empty List of type object
             List<object> newObject = new List<object>();
+            // Add the following values to the list: 7, 28, -1, true, "chair"
             newObject.Add(7);
             newObject.Add(28);
             newObject.Add(-1);
             newObject.Add(true);
             newObject.Add("chair");
 
+            // Loop through the list and print all values (Hint: Type Inference might help here!)
+            // Add all values that are Int type together and output the sum
             int sum = 0;
             for (int i = 0; i < newObject.Count; i++)
             {
