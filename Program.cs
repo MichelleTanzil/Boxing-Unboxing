0c748ba025d3b285b5432f1bf60118dcff1e9371
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-23 15:05:12 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> newObject = new List<object>();
+            newObject.Add(7);
+            newObject.Add(28);
+            newObject.Add(-1);
+            newObject.Add(true);
+            newObject.Add("chair");
+
+            int sum = 0;
+            for (int i = 0; i < newObject.Count; i++)
+            {
+                if (newObject[i] is int) 
+                {
+                    Console.WriteLine($"This is an integer: {newObject[i]}");
+                    int ActualInt = (int)newObject[i];
+                    sum += ActualInt;
+                }
+                else if (newObject[i] is string) 
+                {
+                    Console.WriteLine($"This is an string: {newObject[i]}");
+                }
+                else if (newObject[i] is bool)
+                {
+                    Console.WriteLine($"This is an boolean: {newObject[i]}");
+                }
+            }
+            Console.WriteLine($"The sum of the integers is {sum}.");
+        }
+    }
+}
